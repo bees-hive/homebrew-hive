6a9b368d92410e1c4249acd1b6de3ec06ea0fc65
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2019-10-04 17:41:39 +0300

    Remove `version_schemes` from Elegant Git
    There is a problem with automatic PRs creation (https://stackoverflow.com/q/58181547/10418734). As I can't find a proper solution for it, I just remove `version_schemes`.

--- Formula/elegant-git.rb
+++ Formula/elegant-git.rb
@@ -2,7 +2,6 @@ class ElegantGit < Formula
   desc "Elegant Git is a tool who carefully makes routine work with Git"
   homepage "https://elegant-git.bees-hive.org"
   url "https://github.com/bees-hive/elegant-git.git", :tag => "0.8.0", :revision => "7a5bd5b3a318aeba568f7b467d7364699e6160fb"
-  version_scheme 3
   head "https://github.com/bees-hive/elegant-git.git"
 
   bottle :unneeded
