4864bb0e2c00f01f2f7d363843b50c2d8c8ceb06
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2019-11-25 17:18:00 +0200

    elegant-git 0.15.0

--- Formula/elegant-git.rb
+++ Formula/elegant-git.rb
@@ -1,7 +1,7 @@
 class ElegantGit < Formula
   desc "Elegant Git is a tool who carefully makes routine work with Git"
   homepage "https://elegant-git.bees-hive.org"
-  url "https://github.com/bees-hive/elegant-git.git", :tag => "0.14.0", :revision => "1545b2c060ef07ce784411b497883c1dbcd96044"
+  url "https://github.com/bees-hive/elegant-git.git", :tag => "0.15.0", :revision => "db735f49140d4284a61c763a73143faaf29ac1b8"
   version_scheme 3
   head "https://github.com/bees-hive/elegant-git.git"
 
