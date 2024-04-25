95ccc66ba483cba4e8ca965e52571feeb7055e38
Dmytro Serdiuk <github@extsoft.pro>
2024-04-25 15:29:13 -0400

    Adopt the `elegant-git` to the new Brew 4
    `brew audit --strict --formula elegant-git` passes now.

--- Formula/elegant-git.rb
+++ Formula/elegant-git.rb
@@ -1,11 +1,9 @@
 class ElegantGit < Formula
-  desc "Elegant Git is a tool who carefully makes routine work with Git"
+  desc "Assistant who carefully automates routine work with Git"
   homepage "https://elegant-git.bees-hive.org"
-  url "https://github.com/bees-hive/elegant-git.git", :tag => "0.20.0", :revision => "e47c8471d9f504f3158760ffe797933c779dcecc"
-  version_scheme 3
-  head "https://github.com/bees-hive/elegant-git.git"
+  url "https://github.com/bees-hive/elegant-git.git", tag: "0.20.0", revision: "e47c8471d9f504f3158760ffe797933c779dcecc"
+  head "main"
 
-  bottle :unneeded
   depends_on "bash-completion"
 
   def install
@@ -14,15 +12,16 @@ class ElegantGit < Formula
     zsh_completion.install "completions/_git-elegant" => "_git-elegant"
   end
 
-  def caveats; <<~EOF
-    Please configure Elegant Git by running
+  def caveats
+    <<~EOF
+      Please configure Elegant Git by running
 
-        git-elegant acquire-git
+          git-elegant acquire-git
 
-    If this step is not completed, Elegant Git may behave unexpectedly
-    since the desired Git configuration (relevant for the current version)
-    is not applied.
-  EOF
+      If this step is not completed, Elegant Git may behave unexpectedly
+      since the desired Git configuration (relevant for the current version)
+      is not applied.
+    EOF
   end
 
   test do
