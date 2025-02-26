3fe0110fc6739cd4a9dc862a074b6f77be66f7bc
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2025-02-25 22:32:48 -0600

    aws-creds 0.9.0

--- Formula/aws-creds.rb
+++ Formula/aws-creds.rb
@@ -1,7 +1,7 @@
 class AwsCreds < Formula
   desc "Painless CLI authentication using various AWS identities"
   homepage "https://github.com/bees-hive/aws-creds"
-  url "https://github.com/bees-hive/aws-creds.git", tag: "0.8.0", revision: "af9d2269acce870b2e9bd14f93e168dd177476c9"
+  url "https://github.com/bees-hive/aws-creds.git", tag: "0.9.0", revision: "de9ddb8af8c6f623ba36f4da1463bdde272ac544"
   head "https://github.com/bees-hive/aws-creds.git", branch: "main"
 
   def install
